0859ab49f9c96fbe4cc5a4b3c3c9085a79d610d7
clauR <creyesccf@gmail.com>
2026-03-04 18:38:34 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary < int , string> producto = new Dictionary< int , string>();
+
+            producto.Add(1596, "laptop Gamer");
+            producto.Add(2005, " Mouse inalambrico");
+            producto.Add(2620,"teclado inalambrico");
+
+            Console.WriteLine(" lista de productos ");
+
+            foreach (var item in producto)
+            {
+
+            }
+
+        }
+    }
+}
